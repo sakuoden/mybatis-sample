072d437f0022015eab1de569e529fc2ef4215a6d
作佐部太祐 <sakusabudaisuke@sakusabudaisukenoMacBook-Pro.local>
2021-06-13 13:11:30 +0900

    first commit

---
+++ src/test/java/jtn/sample/MybatisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jtn.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
